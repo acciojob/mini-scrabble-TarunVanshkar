4aab9311d67f4318c3edc3dcb66bb540be6a22f5
TarunVanshkar <117818351+TarunVanshkar@users.noreply.github.com>
2023-05-17 21:46:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 //your code here
+// let inputBox = document.getElementById("evaluatedText");
+// let h3Tag = document.getElementById("letterCount");
+// h3Tag.innerText = 0;
+// function renderCount(){
+//     h3Tag.innerText = inputBox.value.length + 1;
+// }
+// inputBox.addEventListener("keypress", renderCount);
+
 let inputBox = document.getElementById("evaluatedText");
 let h3Tag = document.getElementById("letterCount");
 h3Tag.innerText = 0;
-function renderCount(){
-    h3Tag.innerText = inputBox.value.length + 1;
+function renderCount() {
+    let text = inputBox.value;
+    // if(lastElement !== " ") {
+    // h3Tag.innerText = inputBox.value.length + 1;
+    const textArr = text.split("");
+    let wordCount = 1;
+  
+    for (word of textArr) {
+      if (/[a-zA-Z0-9]/.test(word)) {
+        wordCount += 1;
+      }
+    }
+
+    h3Tag.innerText = wordCount
 }
 inputBox.addEventListener("keypress", renderCount);
