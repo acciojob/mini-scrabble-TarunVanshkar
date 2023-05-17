11e417de0e8c9975598c7c8c393f6b3613fd792c
TarunVanshkar <117818351+TarunVanshkar@users.noreply.github.com>
2023-05-17 21:19:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+let inputBox = document.getElementById("evaluatedText");
+let h3Tag = document.getElementById("letterCount");
+h3Tag.innerText = 0;
+function renderCount(){
+    h3Tag.innerText = inputBox.value.length + 1;
+}
+inputBox.addEventListener("keypress", renderCount);
